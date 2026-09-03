01787414ba7d5f2bc4863329a3e07e2ba48d84ca
tlm <thomas@tlm.id.au>
2026-09-03 04:55:56 +0000

    Fix workshop instance ID CI checks

--- internal/workshop/backend.go
+++ internal/workshop/backend.go
@@ -38,8 +38,8 @@ type ContextKeyUser string
 type ContextKeyWorkshopInstanceID string
 
 const (
-	ContextProjectId          = ContextKeyProjectId("project-id")
-	ContextUser               = ContextKeyUser("user")
+	ContextProjectId = ContextKeyProjectId("project-id")
+	ContextUser      = ContextKeyUser("user")
 	// ContextWorkshopInstanceID stores the identifier of the workshop instance
 	// from which a request originated.
 	ContextWorkshopInstanceID = ContextKeyWorkshopInstanceID("workshop-instance-id")
