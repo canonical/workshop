4003d0c6715169a4e35b11b474d537c3ee34ad9b
Jonathan Conder <jonathan.conder@canonical.com>
2026-09-07 10:15:06 +1200

    Filter waitready signals by name

--- internal/waitready/waitready.go
+++ internal/waitready/waitready.go
@@ -75,7 +75,7 @@ func waitReady(ctx context.Context) error {
 	}
 	defer conn.Close()
 
-	signal := make(chan *dbus.Signal, 1)
+	signal := make(chan *dbus.Signal, 8)
 	conn.Signal(signal)
 
 	options := []dbus.MatchOption{
@@ -98,11 +98,20 @@ func waitReady(ctx context.Context) error {
 	if ready {
 		return nil
 	}
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-signal:
-		return nil
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case sig := <-signal:
+			if sig == nil {
+				return errors.New("bus connection closed unexpectedly")
+			}
+			if sig.Name != "org.freedesktop.systemd1.Manager.StartupFinished" {
+				continue
+			}
+			return nil
+		}
 	}
 }
 
